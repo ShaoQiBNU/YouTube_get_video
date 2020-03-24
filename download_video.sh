868ebc43aa10282c5136666e5bd38a95b01965a6
ShaoQiBNU <37075909+ShaoQiBNU@users.noreply.github.com>
2020-03-24 18:28:02 +0800

    Add files via upload

---
+++ download_video.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "SCS_kb8G4_I&&&Applying sunscreen"
+youtube-dl https://www.youtube.com/watch?v=SCS_kb8G4_I -f best -f mp4 -o './anet_video/SCS_kb8G4_I&&&Applying sunscreen.mp4'
+
+
+echo "x7nDfo6Vm_4&&&Applying sunscreen"
+youtube-dl https://www.youtube.com/watch?v=x7nDfo6Vm_4 -f best -f mp4 -o './anet_video/x7nDfo6Vm_4&&&Applying sunscreen.mp4'
+
+
+echo "vXymIARTD_w&&&Applying sunscreen"
+youtube-dl https://www.youtube.com/watch?v=vXymIARTD_w -f best -f mp4 -o './anet_video/vXymIARTD_w&&&Applying sunscreen.mp4'
